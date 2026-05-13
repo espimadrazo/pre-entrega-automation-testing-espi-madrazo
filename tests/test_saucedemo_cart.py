bcaca83e15f28bbd8f07a7a058924e14bd16e4e3
Espi Madrazo <espimadrazo@gmail.com>
2026-05-13 20:13:11 -0300

    Finished scripts of all tests

--- tests/test_saucedemo_cart.py
+++ tests/test_saucedemo_cart.py
@@ -1,12 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.helpers import login
 
 
 # ADD PRODUCT TO CART
+def test_add_product_to_cart(driver):
 
+    wait = WebDriverWait(driver, 8)
 
-# CHECK CART COUNTER ON HEADER
+    login(driver, "standard_user","secret_sauce")
 
+    catalog_product = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "inventory_item_name"))
+        ).text
 
-# BROWSE TO CART PAGE
+    # Locate and execute "Add to cart" button
+    add_button = wait.until(
+         EC.element_to_be_clickable((By.XPATH,"//button[contains(text(),'Add to cart')]"))
+    )
 
+    add_button.click()
 
-# CHECK PRESENCE OF CORRECT ADDED PRODUCT IN CART
+    # Check cart counter on header icon
+    badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+    assert badge.text == "1"
+
+    # Browse to cart page
+    wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "shopping_cart_link"))
+        ).click()
+    
+    assert "cart.html" in driver.current_url
+
+    # Check presence of correct added product in cart
+    cart_product = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "inventory_item_name"))
+    ).text
+
+    assert cart_product == catalog_product
--- tests/test_saucedemo_catalog.py
+++ tests/test_saucedemo_catalog.py
@@ -1,14 +1,64 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.helpers import login
 
-# CATALOG PAGE TITLE
+
+# CATALOG PAGE TITLE VALIDATION
+def test_inventory_page_title(driver):
+
+    wait = WebDriverWait(driver, 8)
+
+    login(driver, "standard_user","secret_sauce")
 
 
-# CHECK VISIBLE PRODUCTS ON CATALOG PAGE
+    title = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "title"))
+    ).text
+
+    assert title == "Products"
 
 
-# CHECK PRESENCE OF MAIN ELEMENTS: MENU
+# CHECK PRESENCE OF AT LEAST 1 PRODUCT
+def test_product_presence(driver):
+    wait = WebDriverWait(driver, 8)
+
+    login(driver, "standard_user","secret_sauce")
+
+    products = wait.until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[data-test = 'inventory-item']"))
+    )
+
+    assert len(products) >= 1
+
+
+# CHECK PRESENCE OF MAIN ELEMENTS: SIDE MENU
+def test_menu_presence(driver):
+    wait = WebDriverWait(driver, 8)
+
+    login(driver, "standard_user","secret_sauce")
+
+    wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "bm-menu"))
+    )
 
 
 # CHECK PRESENCE OF MAIN ELEMENTS: FILTERS
+def test_menu_presence(driver):
+    wait = WebDriverWait(driver, 8)
 
+    login(driver, "standard_user","secret_sauce")
+
+    wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "product_sort_container"))
+    )
 
 # CHECK PRESENCE OF MAIN ELEMENTS: FOOTER
+def test_menu_presence(driver):
+    wait = WebDriverWait(driver, 8)
+
+    login(driver, "standard_user","secret_sauce")
+
+    wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "footer"))
+    )
